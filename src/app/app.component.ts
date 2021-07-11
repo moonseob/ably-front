038735479e264137903891d1ae58e1ba82caeb75
Moonsub Um <moonsub.um@gmail.com>
2021-07-11 22:17:41 +0900

    fix: fix code that prevents form submit default action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,8 @@ import {
   ChangeDetectionStrategy,
   Component,
   HostListener,
-  OnInit,
 } from '@angular/core';
-import { Store } from '@ngrx/store';
 import { map } from 'rxjs/operators';
-import * as fromRoot from './common/reducers';
 import { RouterDataService } from './common/services/router-data.service';
 
 @Component({
@@ -15,23 +12,18 @@ import { RouterDataService } from './common/services/router-data.service';
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   /** 현재 표시중인게 홈 화면인지 여부 */
   isAtHome$ = this.rdService.getCurrentUrl().pipe(map((url) => url === '/'));
 
-  // SPA 특성상 enter 입력으로 인한 submit 시에도 blur되지 않는 문제를 해결
   @HostListener('keydown.enter', ['$event.target']) onEnterPressed(
     target: EventTarget,
   ) {
     if (target instanceof HTMLInputElement) {
-      target.blur();
+      // SPA 특성상 enter 입력으로 인한 submit 시에도 blur되지 않는 문제를 해결
+      setTimeout(() => target.blur(), 0);
     }
   }
 
-  ngOnInit() {}
-
-  constructor(
-    private rdService: RouterDataService,
-    private store: Store<fromRoot.State>,
-  ) {}
+  constructor(private rdService: RouterDataService) {}
 }
